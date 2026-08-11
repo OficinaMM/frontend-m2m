1f5138ad8f74a2d0817da45bd2b0fc77e791c52a
OficinaMM <administracion@grupom2m.com>
2026-08-11 11:35:29 +0200

    Add new contact information for Efrain Eduardo

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
     'marcelo09vargas90@gmail.com': { nombre: 'Marcelo José', apellidos: 'Vargas López', telefono: '600000011', posicion: 'Oficial de 2ª', dni: 'E28631832' },
     'rojasquinterosrodrigo0@gmail.com': { nombre: 'Rodrigo', apellidos: 'Rojas Quinteros', telefono: '600000012', posicion: 'Peón Especializado', dni: 'Z2561343E' },   
     'rimercamacho48@gmail.com': { nombre: 'Rimer', apellidos: 'Camacho', telefono: '600000012', posicion: 'Oficial de 1ª', dni: 'Z3236151X' },
+    'chiquiedujor@hotmail.com ': { nombre: 'Efrain Eduardo', apellidos: 'Huanca Escalier', telefono: '600000013', posicion: 'Oficial de 1ª', dni: 'X6860106B' },
     'exon.saa0707@gmail.com': { nombre: 'Edson', apellidos: 'Sabino Alvarez Argote', telefono: '600000013', posicion: 'Oficial de 1ª', dni: '54631451B' }
   };
 
